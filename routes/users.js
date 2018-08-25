c25a58db62624fb2eb2a1b267232f404a97c7a16
Ganduri venkata sai dutt <alpha@ganduris-mbp.home>
2018-08-25 01:23:26 -0400

    registration and user model done

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const User = require('../models/user');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
+// const User = require('../models/user');
 
 //Register route
 
-router.get('/register', (req, res, next) => {
-    res.send('This is register screen');
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({success: false, msg: 'Failed to register user'});
+        } else {
+            res.json({success: true, msg: 'Successfully added user'});
+        }
+    });
 });
 
 router.post('/authenticate', (req, res, next) => {
